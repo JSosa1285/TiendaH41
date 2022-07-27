ce7444247f2838b49a08e0ef091cddfef74b4e9a
JSosa1285 <110074640+JSosa1285@users.noreply.github.com>
2022-07-27 00:18:24 -0500

    Initial commit

---
+++ Almacen.java
@@ -0,0 +1,52 @@
+import java.util.List;
+
+public class Almacen
+{
+    private List<Producto> listaProductos;
+    
+    public Almacen()
+    {
+        
+    }
+    
+    public void agregarProdcuto(Producto P)
+    {
+        
+    }
+    
+    public void eliminarProdcuto(int codigo)
+    {
+        
+    }
+    
+    public void aumentarCantProdcuto(int codigo, int cant)
+    {
+        
+    }
+    
+    public void disminuirCantProdcuto(int codigo, int cant)
+    {
+        
+    }
+    
+    public void modificarPrecio(int codigo, int precio)
+    {
+        
+    }
+    
+    public Producto buscarProducto(String criterio)
+    {
+        return null;        
+    }
+    
+    public Producto buscarProducto(int codigo)
+    {
+        return null;        
+    }
+    
+    
+    
+    
+
+
+}
